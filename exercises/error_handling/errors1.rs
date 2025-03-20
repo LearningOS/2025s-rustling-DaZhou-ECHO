906f45474c0592f5d812e8467b7cd0d90488a133
zhouzhou <2821511460@qq.com>
2025-03-20 13:09:14 +0800

    2025/3/20 13:10

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -9,7 +9,6 @@
 // Execute `rustlings hint errors1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn generate_nametag_text(name: String) -> Option<String> {
     if name.is_empty() {
@@ -27,15 +26,20 @@ mod tests {
     #[test]
     fn generates_nametag_text_for_a_nonempty_name() {
         assert_eq!(
-            generate_nametag_text("Beyoncé".into()),
+            generate_nametag_text("Beyoncé".into()).map_or_else(
+                || Err("no data"),
+                | string | Ok(string),
+            ),
             Ok("Hi! My name is Beyoncé".into())
         );
     }
-
     #[test]
     fn explains_why_generating_nametag_text_fails() {
         assert_eq!(
-            generate_nametag_text("".into()),
+            generate_nametag_text("".into()).map_or_else(
+                || Err("`name` was empty; it must be nonempty.".to_string()),
+                | str| Ok(str),
+            ),
             // Don't change this line
             Err("`name` was empty; it must be nonempty.".into())
         );
